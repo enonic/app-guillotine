27fd4cdb010d5fc350985aa763d08c6044351180
Glenn Ricaud <gri@enonic.com>
2017-07-31 15:03:30 +0200

    Headless CMS - Content API (#15)

--- src/main/resources/services/graphql/graphql-content-api.js
+++ src/main/resources/services/graphql/graphql-content-api.js
@@ -1,4 +1,5 @@
 var contentLib = require('/lib/xp/content');
+var portalLib = require('/lib/xp/portal');
 var graphQlLib = require('/lib/graphql');
 
 exports.contentApiType = graphQlLib.createObjectType({
@@ -8,13 +9,40 @@ exports.contentApiType = graphQlLib.createObjectType({
         getSite: {
             type: graphQlLib.reference('Site'),
             args: {
-                key: graphQlLib.nonNull(graphQlLib.GraphQLID)
+                key: graphQlLib.GraphQLID
             },
-            resolve: function(env) {
+            resolve: function (env) {
                 return contentLib.getSite({
-                    key: env.args.key
+                    key: getKey(env)
                 });
             }
+        },
+        getSiteConfig: {
+            type: graphQlLib.GraphQLString,
+            args: {
+                key: graphQlLib.GraphQLID,
+                applicationKey: graphQlLib.nonNull(graphQlLib.GraphQLID)
+            },
+            resolve: function (env) {
+                var config = contentLib.getSiteConfig({
+                    key: getKey(env),
+                    applicationKey: env.args.applicationKey
+                });
+                return config && JSON.stringify(config);
+            }
         }
-    }    
+    }
 });
+
+function getKey(env) {
+    var key = env.args.key;
+    if (!key) {
+        var content = portalLib.getContent();
+        if (content) {
+            key = content._id
+        } else {
+            throw 'Missing field argument key';
+        }
+    }
+    return key;
+}
