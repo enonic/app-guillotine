bd69e9b0d355464ff2085d3c29c2261c603d1efa
Glenn Ricaud <gri@enonic.com>
2017-07-31 15:44:37 +0200

    Headless CMS - Content API (#15)

--- src/main/resources/services/graphql/graphql-content-api.js
+++ src/main/resources/services/graphql/graphql-content-api.js
@@ -2,6 +2,64 @@ var contentLib = require('/lib/xp/content');
 var portalLib = require('/lib/xp/portal');
 var graphQlLib = require('/lib/graphql');
 
+
+var permisionType = graphQlLib.createEnumType({
+    name: 'Permission',
+    description: 'Permission.',
+    values: {
+        'READ': 'READ',
+        'CREATE': 'CREATE',
+        'MODIFY': 'MODIFY',
+        'DELETE': 'DELETE',
+        'PUBLISH': 'PUBLISH',
+        'READ_PERMISSIONS': 'READ_PERMISSIONS',
+        'WRITE_PERMISSIONS': 'WRITE_PERMISSIONS'
+    }
+});
+
+var accessControlEntryType = graphQlLib.createObjectType({
+    name: 'AccessControlEntry',
+    description: 'Access control entry.',
+    fields: {
+        principal: {
+            type: graphQlLib.reference('PrincipalKey'),
+            resolve: function (env) {
+                return env.source.principal;
+            }
+        },
+        allow: {
+            type: graphQlLib.list(permisionType),
+            resolve: function (env) {
+                return env.source.allow;
+            }
+        },
+        deny: {
+            type: graphQlLib.list(permisionType),
+            resolve: function (env) {
+                return env.source.deny;
+            }
+        }
+    }
+});
+var getPermissionsResultType = graphQlLib.createObjectType({
+    name: 'GetPermissionsResult',
+    description: 'Get permissions result.',
+    fields: {
+        inheritsPermissions: {
+            type: graphQlLib.GraphQLBoolean,
+            resolve: function (env) {
+                return env.source.inheritsPermissions;
+            }
+        },
+        permissions: {
+            type: graphQlLib.list(accessControlEntryType),
+            resolve: function (env) {
+                return env.source.permissions;
+            }
+        }
+    }
+});
+
 exports.contentApiType = graphQlLib.createObjectType({
     name: 'ContentApi',
     description: 'Content API',
@@ -29,6 +87,17 @@ exports.contentApiType = graphQlLib.createObjectType({
                     applicationKey: env.args.applicationKey
                 });
                 return config && JSON.stringify(config);
+            }
+        },
+        getPermissions: {
+            type: getPermissionsResultType,
+            args: {
+                key: graphQlLib.GraphQLID
+            },
+            resolve: function (env) {
+                return contentLib.getPermissions({
+                    key: getKey(env)
+                });
             }
         }
     }
