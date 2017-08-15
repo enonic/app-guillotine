e5a342441c6e113ccd658ad1646c3caa68d55603
Glenn Ricaud <gri@enonic.com>
2017-08-15 16:47:57 +0200

    Headless CMS - Schema invalidation (#6)

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -1,29 +1,10 @@
-var eventLib = require('/lib/xp/event');
 var graphQlLib = require('/lib/graphql');
 var schemaLib = require('./schema');
-var namingLib = require('/lib/headless-cms/naming');
-
-
-var schema = null;
-eventLib.listener({
-    type: 'application',
-    localOnly: false,
-    callback: function (event) {
-        if ('STOPPED' === event.data.eventType || 'STARTED' === event.data.eventType) {
-            schema = null;
-            namingLib.resetNameSet();
-        }
-    }
-});
 
 exports.post = function (req) {
     var body = JSON.parse(req.body);
     
-    if (!schema) {
-        schema = schemaLib.createSchema();
-    }
-    
-    var result = graphQlLib.execute(schema, body.query, body.variables);
+    var result = graphQlLib.execute(schemaLib.getSchema(), body.query, body.variables);
     return {
         contentType: 'application/json',
         body: result
--- src/main/resources/services/graphql/schema.js
+++ src/main/resources/services/graphql/schema.js
@@ -1,8 +1,34 @@
+var eventLib = require('/lib/xp/event');
 var graphQlLib = require('/lib/graphql');
+var namingLib = require('/lib/headless-cms/naming');
 var graphQlRootQueryLib = require('./root-query');
 
-exports.createSchema = function () {
+eventLib.listener({
+    type: 'application',
+    localOnly: false,
+    callback: function (event) {
+        if ('STOPPED' === event.data.eventType || 'STARTED' === event.data.eventType) {
+            invalidateSchema();
+        }
+    }
+});
+
+var schema = null;
+exports.getSchema = function () {
+    if (!schema) {
+        schema = createSchema();
+    }
+    return schema;
+};
+
+function createSchema() {
     return graphQlLib.createSchema({
         query: graphQlRootQueryLib.createRootQueryType()
     })
 };
+
+function invalidateSchema() {
+    schema = null;
+    namingLib.resetNameSet();
+}
+
