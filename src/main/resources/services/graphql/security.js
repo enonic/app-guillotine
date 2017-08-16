a9555b1798c06bb1bf0b66164f5d60428ed7962d
Glenn Ricaud <gri@enonic.com>
2017-08-16 13:25:41 +0200

    Headless CMS - Site context (#21)

--- src/main/resources/services/graphql/security.js
+++ src/main/resources/services/graphql/security.js
@@ -1,5 +1,12 @@
 var authLib = require('/lib/xp/auth');
+var portalLib = require('/lib/xp/portal');
 
+exports.isSiteContext = function() {
+    return  !!portalLib.getSite();
+};
+exports.isAllowedSiteContext = function() {
+    return  !!portalLib.getSiteConfig();
+};
 exports.isAdmin = function () {
     log.info('isAdmin:' + authLib.hasRole('system.admin'));
     return authLib.hasRole('system.admin');
