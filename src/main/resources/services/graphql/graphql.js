a9601ae9d9780d8ce52f2246a0c189ff3a19d5c7
GlennRicaud <gri@enonic.com>
2020-01-20 16:25:57 +0100

    Guillotine App - GraphQL WS Protocal & New event subscription #73

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -1,4 +1,7 @@
 var graphQlLib = require('/lib/graphql');
+var graphQlRxLib = require('/lib/graphql-rx');
+var portalLib = require('/lib/xp/portal');
+var webSocketLib = require('/lib/xp/websocket');
 
 var schemaLib = require('/lib/guillotine/schema');
 var securityLib = require('/lib/guillotine/security');
@@ -13,20 +16,105 @@ exports.post = function (req) {
     }
 
     var body = JSON.parse(req.body);
-    var result = graphQlLib.execute(schemaLib.getSchema(req), body.query, body.variables);
+    var result = graphQlLib.execute(schemaLib.getSchema({req: req}), body.query, body.variables);
     return {
         contentType: 'application/json',
         body: result
     };
 };
 
+
 exports.get = function (req) {
+    if (req.webSocket) {
+        return {
+            webSocket: {
+                data: {
+                    branch: req.branch,
+                    siteId: portalLib.getSite()._id
+
+                },
+                subProtocols: ['graphql-ws']
+            }
+        };
+    }
+
     var body = graphqlPlaygroundLib.render();
     return {
         contentType: 'text/html; charset=utf-8',
         body: body
     };
 };
+
+const graphQlSubscribers = {};
+exports.webSocketEvent = function (event) {
+    log.debug('WebSocketEvent: ' + JSON.stringify(event));
+
+    switch (event.type) {
+    case 'close':
+        cancelSubscription(event.session.id);
+        break;
+    case 'message':
+        var message = JSON.parse(event.message);
+        switch (message.type) { //https://github.com/apollographql/subscriptions-transport-ws/blob/master/PROTOCOL.md
+        case 'connection_init':
+            webSocketLib.send(event.session.id, JSON.stringify({
+                type: 'connection_ack'
+            }));
+            break;
+        case 'start':
+            handleStartMessage(event, message);
+            break;
+        case 'stop':
+            cancelSubscription(event.session.id);
+            break;
+        }
+        break;
+    case 'error':
+        log.warn('Session [' + event.session.id + '] error: ' + event.error);
+        break;
+    }
+};
+
+function cancelSubscription(sessionId) {
+    Java.synchronized(() => {
+        const subscriber = graphQlSubscribers[sessionId];
+        if (subscriber) {
+            delete  graphQlSubscribers[sessionId];
+            subscriber.cancelSubscription();
+        }
+    }, graphQlSubscribers)();
+}
+
+function handleStartMessage(event, message) {
+    const sessionId = event.session.id;
+    const graphlqlOperationId = message.id;
+    const payload = message.payload;
+
+    try {
+        const schema = schemaLib.getSchema({
+            branch: event.data.branch,
+            siteId: event.data.siteId
+        });
+        const result = graphQlLib.execute(schema, payload.query, payload.variables);
+
+        if (result.data instanceof com.enonic.lib.graphql.rx.Publisher) {
+            const subscriber = graphQlRxLib.createSubscriber({
+                onNext: (result) => {
+                    webSocketLib.send(sessionId, JSON.stringify({
+                        type: 'data',
+                        id: graphlqlOperationId,
+                        payload: result
+                    }));
+                }
+            });
+            Java.synchronized(() => graphQlSubscribers[sessionId] = subscriber, graphQlSubscribers)();
+            result.data.subscribe(subscriber);
+        }
+    } catch (e) {
+        log.error('Error while handling Start GraphQL-WS message', e);
+        throw e;
+    }
+}
 
 function createNotFoundError() {
     return {
