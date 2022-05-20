837377d166c3caa02fae70cbd9db27a8bff92f58
Anatol Sialitski <asi@enonic.com>
2022-05-20 11:31:11 +0200

    Generate WS url using portalLib #124

--- src/main/resources/admin/widgets/guillotine/guillotine.js
+++ src/main/resources/admin/widgets/guillotine/guillotine.js
@@ -4,9 +4,14 @@ const mustacheLib = require('/lib/mustache');
 exports.get = function (req) {
     const view = resolve('guillotine.html');
     const assetsUrl = portalLib.assetUrl({path: ""});
+    const wsUrl = portalLib.url({
+        path: '/',
+        type: 'websocket'
+    });
 
     const params = {
-        assetsUrl: assetsUrl
+        assetsUrl: assetsUrl,
+        wsUrl: wsUrl.endsWith('/') ? wsUrl.substring(0, wsUrl.length - 1) : wsUrl
     };
     return {
         body: mustacheLib.render(view, params),
