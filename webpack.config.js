c11c2560d75ffd23e37027a56a572f79ba2f9a00
ashklianko <asl@enonic.com>
2022-06-10 16:01:59 +0200

    Set up generation of CSS bundle #138
    -one minified css combining default graphiql and our extended styles for it
    -generating one chunk for js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const webpack = require('webpack');
 const path = require('path');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -8,11 +9,11 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
     context: path.join(__dirname, '/src/main/resources/assets'),
     entry: {
-        'js/main': './js/main.ts'
+        'js/main': './js/main.ts',
+        'styles/main': './styles/main.less',
     },
     output: {
-        path: path.join(__dirname, '/build/resources/main/assets'),
-        filename: './[name].js'
+        path: path.join(__dirname, '/build/resources/main/assets')
     },
     resolve: {
         extensions: ['.ts', '.js', '.less', '.css']
@@ -22,16 +23,23 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: [{loader: 'ts-loader', options: {configFile: 'tsconfig.json'}}]
-            }/*,
+            },
             {
-                test: /\.css$/,
+                test: /\.less$/,
                 use: [
                     {loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}},
                     {loader: 'css-loader', options: {sourceMap: !isProd, importLoaders: 1}},
                     {loader: 'postcss-loader', options: {sourceMap: !isProd}},
                     {loader: 'less-loader', options: {sourceMap: !isProd}},
                 ]
-            }*/
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[base]'
+                }
+            }
         ]
     },
     optimization: {
@@ -45,14 +53,18 @@ module.exports = {
                     keep_classnames: true,
                     keep_fnames: true
                 }
-            })
+            }),
+            new CssMinimizerPlugin()
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: './css/[id].css'
-        })
+        }),
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+        }),
     ],
     mode: isProd ? 'production' : 'development',
     devtool: isProd ? false : 'source-map',
