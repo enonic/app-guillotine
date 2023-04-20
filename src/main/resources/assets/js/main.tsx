61faf3f7cd5c6d7b9a520b631546b17ecf998832
Anatol Sialitski <asi@enonic.com>
2023-04-20 11:11:50 +0200

    Guillotine embedded in Content Studio must "force" white theme #418

--- src/main/resources/assets/js/main.tsx
+++ src/main/resources/assets/js/main.tsx
@@ -12,17 +12,13 @@ const DEFAULT_QUERY = `# Welcome to Query Playground
 # testing GraphQL queries.
 #
 # An example GraphQL query might look like this:
-#
-#     {
-#       guillotine {
-#         getChildren(key: "/") {
-#           displayName
-#         }
-#       }
-#     }
-#
-#
-
+query {
+  guillotine {
+    getChildren(key: "/") {
+      displayName
+    }
+  }
+}
 `;
 
 let currentBranch = 'draft';
@@ -47,6 +43,22 @@ function renderGraphiQLUI() {
     ReactDOM.render(<QueryPlayground/>, getRootContainer(), function () {
         const refreshButton = document.querySelector('[aria-label="Re-fetch GraphQL schema"]');
         refreshButton.addEventListener('click', rerenderGraphiQLUI);
+
+        const bodyElement = document.querySelector('body');
+        bodyElement.classList.remove('graphiql-dark');
+        bodyElement.classList.add('graphiql-light');
+
+        const settingsButton = document.querySelector('[aria-label="Open settings dialog"]');
+        settingsButton.addEventListener('click', function () {
+            setTimeout(function () {
+                const titleElements = document.querySelectorAll('.graphiql-dialog-section-title');
+                titleElements.forEach(function (titleElement) {
+                    if (titleElement.innerHTML === 'Theme') {
+                        titleElement.closest('.graphiql-dialog-section').remove();
+                    }
+                });
+            }, 1);
+        });
     });
 }
 
