c986b76e0fdc526da41aa798bf26288d0f3498b1
Anatol Sialitski <asi@enonic.com>
2026-05-21 16:06:15 +0200

    Keep GraphQL schema available if content type schema generation fails #1318

--- src/main/java/com/enonic/app/guillotine/graphql/ExtensionsProcessor.java
+++ src/main/java/com/enonic/app/guillotine/graphql/ExtensionsProcessor.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLInputObjectField;
@@ -20,6 +23,8 @@ import com.enonic.xp.script.ScriptValue;
 
 public class ExtensionsProcessor
 {
+    private static final Logger LOG = LoggerFactory.getLogger( ExtensionsProcessor.class );
+
     private final GraphQLTypesRegister typesRegister;
 
     public ExtensionsProcessor( final GraphQLTypesRegister typesRegister )
@@ -41,89 +46,204 @@ public class ExtensionsProcessor
 
     private void processEnumTypes( Map<String, ScriptValue> enums )
     {
-        enums.forEach( ( typeName, typeDef ) -> {
-            String description = typeDef.getMember( "description" ).getValue( String.class );
-            ScriptValue valuesDefs = typeDef.getMember( "values" );
+        try
+        {
+            enums.forEach( ( typeName, typeDef ) -> {
+                try
+                {
+                    String description = typeDef.getMember( "description" ).getValue( String.class );
+                    ScriptValue valuesDefs = typeDef.getMember( "values" );
 
-            Map<String, Object> values = new LinkedHashMap<>();
-            valuesDefs.getKeys().forEach( key -> values.put( key, valuesDefs.getMember( key ).getValue() ) );
+                    Map<String, Object> values = new LinkedHashMap<>();
+                    valuesDefs.getKeys().forEach( key -> values.put( key, valuesDefs.getMember( key ).getValue() ) );
 
-            typesRegister.addAdditionalType( GraphQLHelper.newEnum( typeName, description, values ) );
-        } );
+                    typesRegister.addAdditionalType( GraphQLHelper.newEnum( typeName, description, values ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to process GraphQL enum type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "enums", e );
+        }
     }
 
     private void processInputTypes( Map<String, ScriptValue> inputTypes )
     {
-        inputTypes.forEach( ( typeName, typeDef ) -> {
-            String description = typeDef.getMember( "description" ).getValue( String.class );
-            ScriptValue fieldDefs = typeDef.getMember( "fields" );
+        try
+        {
+            inputTypes.forEach( ( typeName, typeDef ) -> {
+                try
+                {
+                    String description = typeDef.getMember( "description" ).getValue( String.class );
+                    ScriptValue fieldDefs = typeDef.getMember( "fields" );
 
-            List<GraphQLInputObjectField> fieldDefinitions = fieldDefs.getKeys().stream().map(
-                fieldName -> GraphQLHelper.inputField( fieldName,
-                                                       CastHelper.cast( fieldDefs.getMember( fieldName ).getValue() ) ) ).collect(
-                Collectors.toList() );
+                    List<GraphQLInputObjectField> fieldDefinitions = fieldDefs.getKeys().stream().map(
+                        fieldName -> GraphQLHelper.inputField( fieldName,
+                                                               CastHelper.cast( fieldDefs.getMember( fieldName ).getValue() ) ) ).collect(
+                        Collectors.toList() );
 
-            typesRegister.addAdditionalType( GraphQLHelper.newInputObject( typeName, description, fieldDefinitions ) );
-        } );
+                    typesRegister.addAdditionalType( GraphQLHelper.newInputObject( typeName, description, fieldDefinitions ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to process GraphQL input type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "inputTypes", e );
+        }
     }
 
     private void processOutputTypes( Map<String, ScriptValue> types )
     {
-        types.forEach( ( typeName, typeDef ) -> {
-            String description = typeDef.getMember( "description" ).getValue( String.class );
-            ScriptValue interfacesDefs = typeDef.getMember( "interfaces" );
-            ScriptValue fieldDefs = typeDef.getMember( "fields" );
+        try
+        {
+            types.forEach( ( typeName, typeDef ) -> {
+                try
+                {
+                    String description = typeDef.getMember( "description" ).getValue( String.class );
+                    ScriptValue interfacesDefs = typeDef.getMember( "interfaces" );
+                    ScriptValue fieldDefs = typeDef.getMember( "fields" );
 
-            List<GraphQLFieldDefinition> fieldDefinitions = extractFields( fieldDefs );
-            List<GraphQLType> interfaces = extractInterfaces( interfacesDefs );
+                    List<GraphQLFieldDefinition> fieldDefinitions = extractFields( fieldDefs );
+                    List<GraphQLType> interfaces = extractInterfaces( interfacesDefs );
 
-            typesRegister.addAdditionalType( GraphQLHelper.newObject( typeName, description, interfaces, fieldDefinitions ) );
-        } );
+                    typesRegister.addAdditionalType( GraphQLHelper.newObject( typeName, description, interfaces, fieldDefinitions ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to process GraphQL output type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "types", e );
+        }
     }
 
     private void processInterfacesTypes( final Map<String, ScriptValue> interfaces )
     {
-        interfaces.forEach( ( typeName, typeDef ) -> {
-            String description = typeDef.getMember( "description" ).getValue( String.class );
-            ScriptValue fieldsDefs = typeDef.getMember( "fields" );
-            List<GraphQLFieldDefinition> fieldDefinitions = extractFields( fieldsDefs );
+        try
+        {
+            interfaces.forEach( ( typeName, typeDef ) -> {
+                try
+                {
+                    String description = typeDef.getMember( "description" ).getValue( String.class );
+                    ScriptValue fieldsDefs = typeDef.getMember( "fields" );
+                    List<GraphQLFieldDefinition> fieldDefinitions = extractFields( fieldsDefs );
 
-            typesRegister.addAdditionalType( GraphQLHelper.newInterface( typeName, description, fieldDefinitions ) );
-        } );
+                    typesRegister.addAdditionalType( GraphQLHelper.newInterface( typeName, description, fieldDefinitions ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to process GraphQL interface type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "interfaces", e );
+        }
     }
 
     private void processUnionsTypes( final Map<String, ScriptValue> unions )
     {
-        unions.forEach( ( typeName, typeDef ) -> {
-            String description = typeDef.getMember( "description" ).getValue( String.class );
-            ScriptValue typesDefs = typeDef.getMember( "types" );
+        try
+        {
+            unions.forEach( ( typeName, typeDef ) -> {
+                try
+                {
+                    String description = typeDef.getMember( "description" ).getValue( String.class );
+                    ScriptValue typesDefs = typeDef.getMember( "types" );
 
-            typesRegister.addAdditionalType( GraphQLHelper.newUnion( typeName, description, CastHelper.cast( typesDefs.getList() ) ) );
-        } );
+                    typesRegister.addAdditionalType(
+                        GraphQLHelper.newUnion( typeName, description, CastHelper.cast( typesDefs.getList() ) ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to process GraphQL union type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "unions", e );
+        }
     }
 
     private void processTypeResolvers( final Map<String, ScriptValue> typeResolvers )
     {
-        typeResolvers.forEach(
-            ( typeName, typeResolver ) -> typesRegister.addTypeResolver( typeName, new DynamicTypeResolver( typeResolver ) ) );
+        try
+        {
+            typeResolvers.forEach( ( typeName, typeResolver ) -> {
+                try
+                {
+                    typesRegister.addTypeResolver( typeName, new DynamicTypeResolver( typeResolver ) );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to register type resolver for GraphQL type '{}'", typeName, e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "typeResolvers", e );
+        }
     }
 
     private void processResolvers( final Map<String, Map<String, ContextualFieldResolver>> resolvers )
     {
-        resolvers.forEach( ( typeName, typeResolver ) -> {
-            Map<String, ContextualFieldResolver> fieldResolvers = resolvers.get( typeName );
-            fieldResolvers.forEach(
-                ( fieldName, fieldResolver ) -> typesRegister.addResolver( typeName, fieldName, new DynamicDataFetcher( fieldResolver ) ) );
-        } );
+        try
+        {
+            resolvers.forEach( ( typeName, typeResolver ) -> {
+                Map<String, ContextualFieldResolver> fieldResolvers = resolvers.get( typeName );
+                fieldResolvers.forEach( ( fieldName, fieldResolver ) -> {
+                    try
+                    {
+                        typesRegister.addResolver( typeName, fieldName, new DynamicDataFetcher( fieldResolver ) );
+                    }
+                    catch ( Exception e )
+                    {
+                        LOG.warn( "Failed to register resolver for GraphQL type '{}' field '{}'", typeName, fieldName, e );
+                    }
+                } );
+            } );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "resolvers", e );
+        }
     }
 
     private void processCreationCallbacks( final Map<String, List<ScriptValue>> creationCallbacksMap )
     {
-        creationCallbacksMap.forEach( ( typeName, creationCallbacks ) -> creationCallbacks.forEach( creationCallback -> {
-            OutputObjectCreationCallbackParams params = new OutputObjectCreationCallbackParams();
-            creationCallback.call( params );
-            typesRegister.addCreationCallback( typeName, params );
-        } ) );
+        try
+        {
+            creationCallbacksMap.forEach( ( typeName, creationCallbacks ) -> creationCallbacks.forEach( creationCallback -> {
+                try
+                {
+                    OutputObjectCreationCallbackParams params = new OutputObjectCreationCallbackParams();
+                    creationCallback.call( params );
+                    typesRegister.addCreationCallback( typeName, params );
+                }
+                catch ( Exception e )
+                {
+                    LOG.warn( "Failed to apply creation callback for GraphQL type '{}'", typeName, e );
+                }
+            } ) );
+        }
+        catch ( Exception e )
+        {
+            LOG.warn( "Failed to process GraphQL schema extensions category '{}'", "creationCallbacks", e );
+        }
     }
 
     private List<GraphQLFieldDefinition> extractFields( ScriptValue fieldDefs )
